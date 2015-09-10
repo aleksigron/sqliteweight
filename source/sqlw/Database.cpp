a92fdb2aa98db6e496734b96f416621611a7244c
Aleksi Grön <aleksi.gron@gmail.com>
2015-09-11 00:08:31 +0300

    Add real sqlw::Database

--- source/sqlw/Database.cpp
+++ source/sqlw/Database.cpp
@@ -1,1 +1,65 @@
 #include "Database.h"
+
+#include "sqlite/sqlite3.h"
+
+namespace sqlw
+{
+	Database::Database() : handle(nullptr)
+	{
+	}
+
+	Database::~Database()
+	{
+		if (this->handle != nullptr)
+			this->Close();
+	}
+
+	bool Database::Open(const char* filename)
+	{
+		if (this->handle == nullptr)
+		{
+			int statusCode = sqlite3_open(filename, &(this->handle));
+
+			if (statusCode == SQLITE_OK)
+				return true;
+
+			// Open did not succeed, but resources were allocated
+			if (this->handle != nullptr)
+			{
+				sqlite3_close(this->handle);
+				this->handle = nullptr;
+			}
+		}
+
+		return false;
+	}
+
+	bool Database::Close()
+	{
+		if (this->handle != nullptr)
+		{
+			int statusCode = sqlite3_close(this->handle);
+
+			if (statusCode == SQLITE_OK)
+			{
+				this->handle = nullptr;
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	bool Database::IsOpen() const
+	{
+		return this->handle != nullptr;
+	}
+
+	int64_t Database::GetLastInsertId() const
+	{
+		if (this->handle != nullptr)
+			return sqlite3_last_insert_rowid(this->handle);
+		else
+			return 0;
+	}
+}
--- source/sqlw/Database.h
+++ source/sqlw/Database.h
@@ -1,9 +1,26 @@
 #pragma once
 
+#include <cstdint>
+
+struct sqlite3;
+
 namespace sqlw
 {
 	class Database
 	{
-		
+	private:
+		sqlite3* handle;
+
+		friend class Statement;
+
+	public:
+		Database();
+		~Database();
+
+		bool Open(const char* filename);
+		bool Close();
+
+		bool IsOpen() const;
+		int64_t GetLastInsertId() const;
 	};
 }
